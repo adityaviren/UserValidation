3cde836d46f6be8f7e864ec52c7eca7a59e7ee06
Aditya Viren <adityaviren@gmail.com>
2020-10-06 16:18:25 +0530

    added check last name

--- src/main/java/com/cg/uservalidation/UserValidation.java
+++ src/main/java/com/cg/uservalidation/UserValidation.java
@@ -7,10 +7,15 @@ public class UserValidation {
     public boolean validateFirstName(String first_name){
         return Pattern.matches("[A-Z]{1}[a-z]{2,}", first_name);
     }
+    public boolean validateLastName(String last_name) {
+        return Pattern.matches("[A-Z]{1}[a-z]{2,}", last_name);
+
+    }
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         UserValidation userValidation = new UserValidation();
         System.out.println(userValidation.validateFirstName(scanner.nextLine()));
+        System.out.println(userValidation.validateLastName(scanner.nextLine()));
     }
 }
