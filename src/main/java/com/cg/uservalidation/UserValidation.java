41c8a11384cd9eb25e0027aaeb08044ab2f00118
Aditya Viren <adityaviren@gmail.com>
2020-10-06 16:16:23 +0530

    added check first name

---
+++ src/main/java/com/cg/uservalidation/UserValidation.java
@@ -0,0 +1,16 @@
+package com.cg.uservalidation;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserValidation {
+    public boolean validateFirstName(String first_name){
+        return Pattern.matches("[A-Z]{1}[a-z]{2,}", first_name);
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        UserValidation userValidation = new UserValidation();
+        System.out.println(userValidation.validateFirstName(scanner.nextLine()));
+    }
+}
