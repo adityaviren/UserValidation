4fa9c165c80d782fa868d4d730cb37b3a29e4c41
Aditya Viren <adityaviren@gmail.com>
2020-10-06 19:27:01 +0530

    modified main to add testclass and modified test class

--- src/main/java/com/cg/uservalidation/UserValidation.java
+++ src/main/java/com/cg/uservalidation/UserValidation.java
@@ -4,18 +4,18 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserValidation {
-    public boolean validateFirstName(String first_name){
+    /*public boolean validateFirstName(String first_name){
         return Pattern.matches("[A-Z]{1}[a-z]{2,}", first_name);
     }
     public boolean validateLastName(String last_name) {
         return Pattern.matches("[A-Z]{1}[a-z]{2,}", last_name);
 
-    }
+    }*/
     public boolean validateEmail(String email) {
-        return Pattern.matches("^[A-Za-z0-9]+(\\.[_A-Za-z0-9-]+)*@"
+        return Pattern.matches("^[A-Za-z0-9]{1}[A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@"
                 + "[A-Za-z0-9]+(\\.[A-Za-z0-9]+){0,1}(\\.[A-Za-z]{2,})$",email);
 
-    }
+    }/*
     public boolean validateMobile(String mobile) {
         return Pattern.matches("[0-9]{2}\s{1}[1-9]{1}[0-9]{9}", mobile);
 
@@ -44,5 +44,5 @@ public class UserValidation {
         System.out.println(userValidation.validateMobile(scanner.nextLine()));
         System.out.println(userValidation.validatePassword(scanner.nextLine()));
         System.out.println(userValidation.happyOrSad(scanner.nextLine()));
-    }
+    }*/
 }
